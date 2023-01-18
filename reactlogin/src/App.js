a041186d117910685eff9adcd00cd8ed3c43a691
Vidhya Mani <vidhyamaniece@gmail.com>
2023-01-18 16:24:56 +0530

    integration of react and rails

---
+++ reactlogin/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <>
+    <h1> Welcome to integration of F.E with B.E</h1>
+    <Signup></Signup>
+    <Dashboard></Dashboard>
+    </>
+  );
+}
+
+export default App;
